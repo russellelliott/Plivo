f78c6980bb127f3dfb55ce3a7bf7a2b5c3c5c844
Russell Elliott <russell@quadsquad.net>
2022-08-23 16:48:00 -0700

    figuring out geolocation

--- src/components/UserLocation/Location.js
+++ src/components/UserLocation/Location.js
@@ -2,11 +2,15 @@ import {KEY} from "./GoogleMapsAPI"
 
 import { useLoadScript } from "@react-google-maps/api";
 import Map from "./Map";
+import Geolocation from "./Geolocation"
 
 export default function App() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: KEY // Add your API key
   });
 
-  return isLoaded ? <Map /> : null;
+  return(
+    isLoaded ? <Map /> : null
+    //<div><Geolocation/></div>
+  );
 }
