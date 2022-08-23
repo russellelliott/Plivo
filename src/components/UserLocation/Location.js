451ddcbfabae6085ea112caddce421ae83a9addb
Russell Elliott <russell@quadsquad.net>
2022-08-23 14:38:29 -0700

    map displays properly

--- src/components/UserLocation/Location.js
+++ src/components/UserLocation/Location.js
@@ -1,23 +1,12 @@
 import {KEY} from "./GoogleMapsAPI"
 
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { useLoadScript } from "@react-google-maps/api";
+import Map from "./Map";
 
-export default function Home() {
+export default function App() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: KEY,
+    googleMapsApiKey: KEY // Add your API key
   });
 
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
-}
-
-function Map() {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
-
-  return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
-    </GoogleMap>
-  );
+  return isLoaded ? <Map /> : null;
 }
