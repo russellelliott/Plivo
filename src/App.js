acc4904a4f5d5c89cd387f1127f27ca80930c72e
Russell Elliott <russell@quadsquad.net>
2022-08-23 10:35:00 -0700

    added google authentication and login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+//For the router/navbar stuff
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login"; //login page
+
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
+
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+  
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <nav>
+            {!isAuth ? (
+              <Link to="/login"> Login </Link>
+            ) : (
+              <>
+                  <Link to="/"> Home </Link>
+                  <Link to="/profile"> Profile</Link>
+                  <Link to="/videocall">Video Call</Link>
+                  <button onClick={signUserOut}> Log Out</button>
+              </>
+            )}
+      </nav>
+        <Routes>
+              
+              <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+              
+        </Routes>
+      </Router>
   );
 }
 
