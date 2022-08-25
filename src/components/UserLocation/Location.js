8224e61438eee73abcbf12a296d3ab1873f3c991
Russell Elliott <russell@quadsquad.net>
2022-08-25 15:44:52 -0700

    display geodata instead of map

--- src/components/UserLocation/Location.js
+++ src/components/UserLocation/Location.js
@@ -10,7 +10,7 @@ export default function App() {
   });
 
   return(
-    isLoaded ? <Map /> : null
-    //<div><Geolocation/></div>
+    //isLoaded ? <Map /> : null
+    <div><Geolocation/></div>
   );
 }
