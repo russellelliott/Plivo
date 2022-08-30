90b8e90a69b694bc47c5010779e5f824fbb2c4ac
Agustin Escoda <32514472+Anform@users.noreply.github.com>
2022-08-29 20:14:33 -0400

    Added admin option, uploads to firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import { auth, provider } from "../firebase-config";
+import React, {useEffect, useState} from 'react';
+import { auth, provider,db} from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import {addDoc, collection, getDocs} from "firebase/firestore"
 
-function Login({ setIsAuth }) {
+function Login({ setIsAuth, setUser, currentUser }) {
+  
+    const [isAdmin, setIsAdmin] = useState(false)
+    const [data, setData] = useState([])
+    const dataCollectionRef = collection(db,"users")
     let navigate = useNavigate();
+
+    useEffect(() => {
+      const getData = async () => {
+          const info = await getDocs(dataCollectionRef);
+          setData(info.docs.map((doc) => ({...doc.data(), id: doc.id })))
+      };
+      getData();
+  }, []);
+
+    const handleChange = event => {
+      if(event.target.checked) {
+        setIsAdmin(true)
+      }
+      else {
+        setIsAdmin(false)
+      }
+    }
   
     const signInWithGoogle = () => {
       signInWithPopup(auth, provider).then((result) => {
+        setUser({email: result.user.email, name: result.user.displayName, admin: isAdmin})
         localStorage.setItem("isAuth", true);
         setIsAuth(true);
+        var check = false
+        if(isAdmin === true) {
+          data.map((data) => {
+            if(data.email === result.user.email)
+            {
+              check = true
+            }
+          })
+          if(check === false) {
+             addDoc(dataCollectionRef, { email: result.user.email, name: result.user.displayName, admin: isAdmin})
+          }
+          else
+          {
+            console.log("User already registered")
+          }
+        }
         navigate("/");
       });
     };
@@ -20,8 +59,12 @@ function Login({ setIsAuth }) {
         <button className="login-with-google-btn" onClick={signInWithGoogle}>
           Sign in with Google
         </button>
+        <label>Check box if admin
+            <input type = "checkbox" value = {isAdmin} onChange = {handleChange}/>
+        </label>
       </div>
     );
   }
+
   
   export default Login;
