7f08e71e84f2a5230157c1667ea9e4dd5cc7a638
Agustin Escoda <32514472+Anform@users.noreply.github.com>
2022-08-30 23:48:03 -0400

    Full functionality + Style

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,9 +1,25 @@
 import emailjs from '@emailjs/browser';
-import { auth, db} from "../firebase-config";
-import React, { useRef } from 'react'
-import { Button, Modal } from 'react-bootstrap'
+import { db } from "../firebase-config";
+import {collection, getDocs} from "firebase/firestore"
+import React, { useState,useRef, useEffect } from 'react'
+import "./index.css"
+import { clear } from '@testing-library/user-event/dist/clear';
 function Email({currentUser}) {
   const form = useRef();
+  const [message,setMessage] = useState("")
+  const [senderEmail, setSenderEmail] = useState("")
+  const [adminName, setAdminName] = useState("")
+  const [data, setData] = useState([])
+  const [subject, setSubject] = useState("")
+  const dataCollectionRef = collection(db, "users")
+  
+  useEffect(() => {
+    const getData = async () => {
+        const info = await getDocs(dataCollectionRef);
+        setData(info.docs.map((doc) => ({...doc.data(), id: doc.id })))
+    };
+    getData();
+}, []);
 
   const sendEmail = (e) => {
     e.preventDefault()
@@ -14,30 +30,45 @@ function Email({currentUser}) {
         console.log(error.text);
     });
     e.target.reset()
+    clearInputs()
   }
 
-  function EmailModal(props) {
-    return (
-      <Modal>
-
-      </Modal>
-    )
+  const clearInputs = () => {
+    setMessage("")
+    setSenderEmail("")
+    setAdminName("")
+    setSubject("")
   }
 
-  
   return (
     <>
+      <h1 style={{textAlign:'center'}}>Select an admin</h1>
+      <h4 style={{textAlign:'center'}}>Currently selected admin: {adminName}</h4>
+      {data.filter((info) => {
+        if(info.admin == true)
+        {
+          return info
+        }
+      }).map((info) => {
+        return (
+          <div style={{display:"flex", justifyContent:"center", margin:"10px"}}>
+            <button onClick = {(e) => {setSenderEmail(info.email); setAdminName(info.name)}}>{info.name}</button>
+          </div>
+        )
+      })}
       <div>
-        <form className = "form" ref = {form} onSubmit = {sendEmail}>
+        <form className = "--form-control --card --flex-center --dir-column" ref = {form} onSubmit = {((sendEmail))}>
           <input type = "hidden" value = {currentUser.name} name = 'from_name'></input>
           <input type = "hidden" value = {currentUser.email} name = 'from_email'></input>
-          <input type = "email" placeholder='To Email' name = 'to_email'></input>
-          <input type = "text" placeholder='Subject...' name = "subject"></input>
-          <textarea name = "message"></textarea>
-          <button type = "submit">Send Message</button>
+          <input type = "hidden" value = {senderEmail} name = 'to_email'></input>
+          <input type = "text" placeholder='Subject...' name = "subject" value = {subject} onChange = {(e)=>{setSubject(e.target.value)}}></input>
+          <textarea className='--text-sm' onChange={(e)=>{setMessage(e.target.value)}} cols="100" rows="15" name = "message" value = {message}></textarea>
+          
+          <button className='--btn --btn-primary' type = "submit">Send Message</button>
+          <button className='--btn --btn-danger'>Clear Page</button>
+          
         </form>
       </div>
-      <button onClick={(e) => {console.log(currentUser)}}>Test</button>
     </>
   );
 }
