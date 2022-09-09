49cc174a975e333e2f6686c75e66beac76cc5ca0
Agustin Escoda <32514472+Anform@users.noreply.github.com>
2022-09-09 13:57:33 -0400

    Added Inbox/Reply Functionality

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import Login from "./components/Login"; //login page
 import VideoDashboard from "./components/VideoCall/VideoDashboard"; //video dashboard page
 import Location from "./components/UserLocation/Location" //user location
 import Photo from "./components/Photo" //take photo
+import Display from "./components/Display"
 
 import { useEffect, useState } from "react";
 import { signOut, getAuth, onAuthStateChanged } from "firebase/auth";
 import { auth,db } from "./firebase-config";
 import Email from './components/Email';
+import Inbox from "./components/Inbox"
+// chat messaging
+import Home from "./components/Home"
+import ChatHome from './components/ChatRoom/ChatHome';
+import SupportAdmin from './components/SupportEngine/SupportAdmin';
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
@@ -48,18 +54,23 @@ function App() {
                   <Link to="/videocall">Video Call</Link>
                   <Link to="/location">Location</Link>
                   <Link to="/photo">Photo</Link>
-                  <Link to="/email">Email</Link>
+                  <Link to="/Inbox">Inbox</Link>
+                  <Link to="/global">GlobalChat</Link>
                   <button onClick={signUserOut}> Log Out</button>
               </>
             )}
       </nav>
         <Routes>
-              
+              <Route path='/' element={<Home />} />
               <Route path="/login" element={<Login setIsAuth={setIsAuth}/>}/>
+              <Route path="/global" element={<ChatHome />} />
               <Route path="/videocall" element={<VideoDashboard />} />
               <Route path="/location" element={<Location />} />
               <Route path="/photo" element={<Photo />} />
               <Route path="/email" element={<Email currentUser = {currentUser}/>} />
+              <Route path="/support" element={<SupportAdmin />} />
+              <Route path = "/inbox" element={<Inbox currentUser = {currentUser}/>}/>
+              <Route path = "display" element={<Display></Display>}></Route>
         </Routes>
       </Router>
   );
